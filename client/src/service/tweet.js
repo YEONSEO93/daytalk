1023f21e3ca215448367ae9e60589849bd6205fd
Yeonseo Ko <yeooonseo@gmail.com>
2024-12-02 21:09:55 +1000

    client updated

--- client/src/service/tweet.js
+++ client/src/service/tweet.js
@@ -1,56 +1,32 @@
 export default class TweetService {
-  
-  constructor(baseURL = 'http://localhost:8080') {
-  this.baseURL = baseURL;
-}
+  constructor(http) {
+    this.http = http;
+  }
 
   async getTweets(username) {
-    let query = username ? `?username=${username}` : '';
-    const response = await fetch(`${this.baseURL}/talks${query}`, {
+    const query = username ? `?username=${username}` : '';
+    return this.http.fetch(`/talks${query}`, {
       method: 'GET',
-      headers: {'Content-Type': 'application/json' },
     });
-    const data = await response.json();
-    if(response.status !==200) {
-      throw new Error(data.message);
-    }
-    return data;
   }
 
   async postTweet(text) {
-    const response = await fetch(`${this.baseURL}/talks`, {
+    return this.http.fetch(`/talks`, {
       method: 'POST',
-      headers: {'Content-Type': 'application/json' },
-      body: JSON.stringify({ text, username: 'yeonseo', name: 'YEONSEO'})
+      body: JSON.stringify({ text, username: 'YEONSEO', name: 'YEONSEO' }),
     });
-    const data = await response.json();
-    if(response.status !==201) {
-      throw new Error(data.message);
-    }
-    return data;
   }
 
   async deleteTweet(talkId) {
-    const response = await fetch(`${this.baseURL}/talks/${talkId}`, {
+    return this.http.fetch(`/talks/${talkId}`, {
       method: 'DELETE',
-      headers: {'Content-Type': 'application/json' },
     });
-    if (response.status !== 204) {
-      const data = await response.json(); // Parse response body in case of error
-      throw new Error(data.message);
-    }
   }
 
   async updateTweet(talkId, text) {
-    const response = await fetch(`${this.baseURL}/talks/${talkId}`, {
+    return this.http.fetch(`/talks/${talkId}`, {
       method: 'PUT',
-      headers: {'Content-Type': 'application/json' },
-      body: JSON.stringify({ text})
+      body: JSON.stringify({ text }),
     });
-    const data = await response.json();
-    if(response.status !==200) {
-      throw new Error(data.message);
-    }
-    return data;
   }
 }
