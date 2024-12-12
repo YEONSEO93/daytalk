b40c56729c06223ef3353048662f96bbf431e343
Yeonseo Ko <yeooonseo@gmail.com>
2024-12-12 13:14:38 +1000

    Validation

--- server/router/talks.js
+++ server/router/talks.js
@@ -1,7 +1,17 @@
 import express from 'express';
 import * as talkController from '../controller/talk.js';
+import {body} from 'express-validator';
+import {validate} from '../middleware/validator.js';
 
 const router = express.Router();
+
+const validateTalk = [
+  body('text')
+  .trim()
+  .isLength({min: 3})
+  .withMessage('text should be at least 3 characters'),
+  validate
+];
 
 // GET /talks
 // GET /talks?username=:username
@@ -10,11 +20,12 @@ router.get('/', talkController.getTalks);
 // GET /talks/:id
 router.get('/:id', talkController.getTalk);
 
+
 // POST /talks
-router.post('/', talkController.createTalk);
+router.post('/', validateTalk, talkController.createTalk);
 
 // PUT /talks/:id
-router.put('/:id', talkController.updateTalk);
+router.put('/:id', validateTalk, talkController.updateTalk);
 
 // DELETE /talks/:id
 router.delete('/:id', talkController.deleteTalk);
